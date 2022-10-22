9167469ea80d8fa9247cedab44699f6fc684a78d
Saket <blend.saketkumar@gmail.com>
2022-10-22 12:37:46 +0530

    Multiplication

---
+++ scarf.py
@@ -0,0 +1,84 @@
+# amt = float(input("Enter amount: "))
+# interest = float(input("Enter interest"))
+# period = int(input("Enter years"))
+# value = 0
+# year = 1
+# while year<= period:
+#     value = amt + amt*interest
+#     print(f"Your amt in {year} is {value}")
+#     amt = value
+#     year = year + 1
+
+# N = 10
+# sum = 0
+# n = 0
+# while n<N:
+#     no = float(input("enter"))
+#     sum = no + sum
+#     n = n+1
+# avg = float(sum)/N
+# print(avg)
+
+#
+# sum = 0.0
+# for i in range(1,11):
+#     sum = sum + 1/i
+#     print(f"{i}, {sum}")
+
+
+
+# import math
+# a = int(input("enter"))
+# b = int(input("enter"))
+# c = int(input("enter"))
+# d= b*b - 4*a*c
+# if d<0:
+#     print("roots are imaginary")
+# else:
+#     r1 = (-b + math.sqrt(d))/(2*a)
+#     r2 = (-b - math.sqrt(d))/(2*a)
+#     print(r1)
+#     print(r2)
+
+# #Fibonnacci Series........
+# a,b = 0,1
+# while b<100:
+#     print(b)
+#     a,b=b,a+b
+
+
+
+n=20
+print("-"*50)
+for i in range(1,11):
+    print(f"{n}x{i}={n*i}", end=" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
